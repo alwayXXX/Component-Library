d670a5c96cf5c971358033af48f07087f307cb69
alwayXXX <513805757@qq.com>
2024-10-18 21:14:26 +0800

    MVP/UnoCss/VitePress

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import { presetUno, presetAttributify, presetIcons } from "unocss";
+import Unocss from "./config/unocss";
+
+const rollupOptions = {
+    external: ["vue", "vue-router"],
+    output: {
+        globals: {
+            vue: "Vue",
+        },
+    },
+};
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        vueJsx(),
+        Unocss(),
+    ],
+    build: {
+        rollupOptions,
+        minify: false,
+        cssCodeSplit:true,
+        // 添加库模式配置
+        lib: {
+            entry: "./src/entry.ts",
+            name: "SSYUI",
+            fileName: "alwayxx-ui",
+            // 导出模块格式
+            formats: ["es", "umd", "iife"],
+        },
+    },
+});
